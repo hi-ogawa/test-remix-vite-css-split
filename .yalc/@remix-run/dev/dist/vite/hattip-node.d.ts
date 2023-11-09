32b01da3ee1a75ea543e9c3fca6edd45a5923f8b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-09 13:46:38 +0900

    fix: fix code split css

---
+++ .yalc/@remix-run/dev/dist/vite/hattip-node.d.ts
@@ -0,0 +1,4 @@
+export declare const createHattipNodeHandler: (args_0: import("@remix-run/server-runtime").ServerBuild, args_1: {
+    mode?: string | undefined;
+    criticalCss?: string | undefined;
+}) => import("@hattip/adapter-node/native-fetch").NodeMiddleware;
